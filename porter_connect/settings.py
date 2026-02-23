862ad0f324b7f548dbe5bfa190f2e1d40a2aa9e6
Akmal Naiem Ansari <akmalnaiem25@gmail.com>
2026-02-24 01:05:16 +0530

    Updated settings file

--- porter_connect/settings.py
+++ porter_connect/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost",
+    cast=lambda v: [s.strip() for s in v.split(",")],
+)
 
 
 # Application definition
