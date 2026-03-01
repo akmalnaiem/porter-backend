8e0ef59b7c068cd62eccca215c9e880a9455e24e
Akmal Naiem Ansari <akmalnaiem25@gmail.com>
2026-03-01 14:12:10 +0530

    enable admin on staging

--- porter_connect/urls.py
+++ porter_connect/urls.py
@@ -19,11 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/v1/accounts/', include("accounts.api.v1.urls")),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += [
-        path('admin/', admin.site.urls),
-    ]
