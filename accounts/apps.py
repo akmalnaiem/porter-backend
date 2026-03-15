bc772f943d1f97555e183b28a4530af30d8704c0
Akmal Naiem Ansari <akmalnaiem25@gmail.com>
2026-03-16 01:45:00 +0530

    remove the superuser creds

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,21 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
 
 
 class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-
-        User = get_user_model()
-
-        try:
-            if not User.objects.filter(email="akmalnaiem25@gmail.com").exists():
-                User.objects.create_superuser(
-                    email="akmalnaiem25@gmail.com",
-                    phone_number="7828807574",
-                    password="zzpp57t83DaBms"
-                )
-        except Exception:
-            pass
